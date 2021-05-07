ac27c517ba63e8be20602295a1c1d07480f4c185
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-08 01:05:42 +0200

    implement movies endpoints

--- routes/movies.py
+++ routes/movies.py
@@ -1,43 +1,72 @@
 from flask import jsonify, Blueprint
 from auth import requires_auth
+from models.movie import Movie
 bp = Blueprint('movies', __name__, url_prefix='/movies')
 
 
 @bp.route('/', methods=['GET'])
 @requires_auth('get:movies')
 def get_movies():
-    # TODO//: implemet endpoint
+    page = request.args.get('page', default='1', type=int)
+    per_page = request.args.get('per_page', default='10', type=int)
+    movies = movies.query.paginate(
+        page=page, per_page=per_page)
     return jsonify({
-        "success": False,
-        "error": "not implemented"
+        "success": True,
+        "movies": [
+            movie.toDict() for movie in movies
+        ],
+        'code': 200
     }), 200
 
 
-@bp.route('/', methods=['DELETE'])
+@bp.route('/<id:int>', methods=['DELETE'])
 @requires_auth('delete:movies')
-def delete_movies():
-    # TODO//: implemet endpoint
+def delete_movies(id):
+    movie = Movie.query.get(id)
+    db.session.delete(movie)
+    db.session.commit()
     return jsonify({
-        "success": False,
-        "error": "not implemented"
+        "success": True,
+        "actors": [movie.toDict()]
+        'code': 200,
     }), 200
 
 
 @bp.route('/', methods=['POST'])
 @requires_auth('add:movies')
 def post_movies():
-    # TODO//: implemet endpoint
+    json_data = request.json
+    title = json_data['title']
+    release_date = json_data['release_date']
+    new_movie = Movie(title=title, release_date=release_date)
+    session = db.session
+    try:
+        db.session.add(new_actor)
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        return jsonify({
+            "success": False,
+            "error": Exception.__repr__(),
+            'code': 500,
+        }), 500
+    finally:
+        db.session.close()
+
     return jsonify({
-        "success": False,
-        "error": "not implemented"
-    }), 200
+        "success": True,
+        "movies": [new_actor.toDict()]
+    }), 201
 
 
 @bp.route('/', methods=['PATCH'])
 @requires_auth('patch:movies')
 def patch_movies():
-    # TODO//: implemet endpoint
+    json_data = request.json
+    movie = Movie.query.get(id)
+    Movie.update(json_data)
     return jsonify({
-        "success": False,
-        "error": "not implemented"
+        "success": True,
+        "movies": [actor.toDict()]
     }), 200
