d2b04c9147b0e33638efd0897f9764d7ac8be0c1
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-07 06:11:00 +0200

    add movie model

---
+++ models/movie.py
@@ -0,0 +1,10 @@
+from app.db import db
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=False, nullable=False)
+    release_date = db.Column(db.DateTime, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Movie {self.id},{self.title},{self.release_date}>'
