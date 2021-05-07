4d75e33f86a07f58859c9a87610d80db80e43e7b
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-08 00:32:11 +0200

    implement get_actors endpoint

--- routes/actors.py
+++ routes/actors.py
@@ -1,15 +1,22 @@
 from flask import jsonify, Blueprint
 from auth import requires_auth
+from models.actor import Actor
 bp = Blueprint('actors', __name__, url_prefix='/actors')
 
 
 @bp.route('/', methods=['GET'])
 @requires_auth('get:actors')
 def get_actors():
-    # TODO//: implemet endpoint
+    page = request.args.get('page', default='1', type=int)
+    per_page = request.args.get('per_page', default='10', type=int)
+    actors = Actor.query.paginate(
+        page=page, per_page=per_page)
     return jsonify({
-        "success": False,
-        "error": "not implemented"
+        "success": True,
+        "actors": [
+            actor.toDict() for actor in actors
+        ],
+        'code': 200
     }), 200
 
 
