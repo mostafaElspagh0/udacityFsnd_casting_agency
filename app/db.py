a468eba49626e0b6b9e023c6fbbb0176f7675fc5
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-07 06:04:57 +0200

    create actor model

--- app/db.py
+++ app/db.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db: SQLAlchemy() = SQLAlchemy()
+
 
 def setup_db(app):
     db.app = app
---
+++ models/actor.py
@@ -0,0 +1,11 @@
+from app.db import db
+
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    age = db.Column(db.Integer, unique=False, nullable=False)
+    gender = db.Column(db.String(30), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Actor {self.id},{self.name},{self.age},{self.gender}>'
