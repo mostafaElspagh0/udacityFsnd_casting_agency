eff154c300035c0560372767a998db234f48b2e0
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-08 00:12:20 +0200

    implement auth

--- config.py
+++ config.py
@@ -21,7 +21,12 @@ class DatabaseUri:
 
 HELLOWORLD = "hello from config"
 
-
 SQLALCHEMY_DATABASE_URI = str(DatabaseUri())
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+
+ALGORITHMS = [i.strip() for i in os.getenv("ALGORITHMS"), split(",")]
+
+API_AUDIENCE = os.getenv("API_AUDIENCE")
