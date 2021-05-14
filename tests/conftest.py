73c708120e74a40366d24553900556e979a4dc06
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-14 10:24:11 +0200

    refactor tests into their own package

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import os
-from utils import login_util
+from tests.utils import login_util
 from flask import Flask, Response
 from flask.wrappers import Request
 import pytest
@@ -30,13 +30,3 @@ def executive_producer_token():
     username = os.getenv("EXECUTIVE_PRODUCER_USER")
     password = os.getenv("EXECUTIVE_PRODUCER_PWD")
     return login_util(username, password)
-
-
-def test_root_endpoint_return_healthy(client: Flask):
-    rv: Response = client.get('/')
-    assert rv.status_code == 200
-
-
-def test_get_actors_endpoint_return_200(client: Flask):
-    rv: Response = client.get('/actors')
-    assert rv.status_code == 200
