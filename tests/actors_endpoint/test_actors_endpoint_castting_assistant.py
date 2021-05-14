2c464456a60a6773e39229f224398665e5b36512
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-14 12:19:28 +0200

    fix typo in test name

--- tests/actors_endpoint/test_actors_endpoint_castting_assistant.py
+++ tests/actors_endpoint/test_actors_endpoint_castting_assistant.py
@@ -3,7 +3,7 @@ from flask.app import Flask
 from flask.wrappers import Response
 
 
-def test_get_actors_endpoint_return_401_if_castting_assistant(client: Flask, casting_assistant_headers):
+def test_get_actors_endpoint_return_200_if_castting_assistant(client: Flask, casting_assistant_headers):
     rv: Response = client.get('/actors', headers=casting_assistant_headers)
     assert rv.status_code == 200
     assert rv.content_type == "application/json"
