93aea5ee840339baac739369a3e4e6ccee1026ee
mostafaElspagh0 <mostafaelspagh0@gmail.com>
2021-05-14 09:46:08 +0200

    create fixture for tokens for every role

--- unit_test.py
+++ unit_test.py
@@ -1,4 +1,7 @@
-from flask import Flask,Response
+import os
+from utils import login_util
+from flask import Flask, Response
+from flask.wrappers import Request
 import pytest
 from app import create_app
 
@@ -8,9 +11,31 @@ def client():
     return create_app().test_client()
 
 
-def test_root_endpoint_return_404(client: Flask):
+@pytest.fixture
+def casting_assistant_token():
+    username = os.getenv("CASTING_ASSISTANT_USER")
+    password = os.getenv("CASTING_ASSISTANT_PWD")
+    return login_util(username, password)
+
+
+@pytest.fixture
+def casting_director_token():
+    username = os.getenv("CASTING_DIRECTOR_USER")
+    password = os.getenv("CASTING_DIRECTOR_PWD")
+    return login_util(username, password)
+
+
+@pytest.fixture
+def executive_producer_token():
+    username = os.getenv("EXECUTIVE_PRODUCER_USER")
+    password = os.getenv("EXECUTIVE_PRODUCER_PWD")
+    return login_util(username, password)
+
+
+def test_root_endpoint_return_healthy(client: Flask):
     rv: Response = client.get('/')
-    assert rv.status_code == 404
+    assert rv.status_code == 200
+
 
 def test_get_actors_endpoint_return_200(client: Flask):
     rv: Response = client.get('/actors')
